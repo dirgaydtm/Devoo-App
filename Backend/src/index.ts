42b8473a480d4be0289624a6f92e0bbacd77b997
Dirgaaa <dirgayuditama6@gmail.com>
2025-09-01 07:59:26 +0700

    chore: fix file permissions

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import { connectDB } from './lib/db';
 import authRoutes from './routes/auth.route';
@@ -12,6 +13,10 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
