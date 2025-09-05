9866280619ad7525d257e1f9ef4580ea69677dfd
Dirgaaa <dirgayuditama6@gmail.com>
2025-09-05 20:44:04 +0700

    feat(auth): complete auth store (signup, login, logout, checkAuth)

--- Frontend/src/store/useAuthStore.ts
+++ Frontend/src/store/useAuthStore.ts
@@ -1,10 +1,17 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+import { AxiosError } from "axios";
 
 interface User {
   id: string;
   username: string;
   email: string;
+}
+interface data {
+  username?: string;
+  email: string;
+  password: string;
 }
 
 interface AuthStore {
@@ -14,6 +21,9 @@ interface AuthStore {
   isUpdatingProfile: boolean;
   isCheckingAuth: boolean;
   checkAuth: () => Promise<void>;
+  signup: (data: data) => Promise<void>;
+  logout: () => Promise<void>;
+  login: (data: data) => Promise<void>;
 }
 
 export const useAuthStore = create<AuthStore>((set) => ({
@@ -34,11 +44,46 @@ export const useAuthStore = create<AuthStore>((set) => ({
     } finally {
       set({ isCheckingAuth: false });
     }
+  },
+
+  signup: async (data: data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (err) {
+      const error = err as AxiosError<{ message: string }>;
+      toast.error(error.response?.data?.message || "Error signing up");
+    } finally {
+      set({ isSigningUp: false });
+    }
+
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (err) {
+      const error = err as AxiosError<{ message: string }>;
+      toast.error(error.response?.data?.message || "Error logging out");
+
+    }
+  },
+  login: async (data: data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    } catch (err) {
+      const error = err as AxiosError<{ message: string }>;
+      toast.error(error.response?.data?.message || "Error logging in");
+    } finally {
+      set({ isLoggingIn: false });
+    }
+
   }
-
-  //  signUp: async (data) => {
-
-  // }
-
-
 }))
