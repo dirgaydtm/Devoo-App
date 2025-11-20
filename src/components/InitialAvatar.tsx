454972c16004e8fef1fb51b26c55456dfa0b7277
Dirgaaa <dirgayuditama6@gmail.com>
2025-11-20 22:15:03 +0700

    refactor: share avatar initials

--- src/components/InitialAvatar.tsx
+++ src/components/InitialAvatar.tsx
@@ -1,15 +1,12 @@
 import { useAuthStore } from "../store/useAuthStore"
+import { getUserInitials } from "../lib/user"
 
 const Avatar = () => {
     const { authUser } = useAuthStore();
-    const initial = authUser?.username
-        ?.split(" ")
-        .map(word => word[0])
-        .join("")
-        .toUpperCase() || "?"
+    const initial = getUserInitials(authUser?.username)
 
     return (
-        <div className="size-full rounded-full text-5xl bg-gradient-to-r from-primary to-secondary flex items-center justify-center font-bold shadow-md">
+        <div className="size-full rounded-full text-5xl bg-linear-to-r from-primary to-secondary flex items-center justify-center font-bold shadow-md">
             {initial}
         </div>
     )
